19d54390446e7f225c6ad3e2c23409d869415a54
Navinsuvarna8355 <156652548+Navinsuvarna8355@users.noreply.github.com>
2025-08-22 19:01:38 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,7 +1,9 @@
-import pandas as pd
+import ta
 
 def generate_signals(close_series):
-    ema = pd.Series(close_series).ewm(span=10).mean().iloc[-1]
-    signal = "BUY" if close_series[-1] > ema else "SELL"
-    reason = "Close > EMA (bullish)" if signal == "BUY" else "Close < EMA or bearish trend"
+    ema = ta.trend.ema_indicator(close_series, window=21).iloc[-1]
+    stc = ta.trend.STCIndicator(close=close_series, fillna=True).stc().iloc[-1]
+
+    signal = "BUY" if close_series.iloc[-1] > ema and stc > ema else "SELL"
+    reason = "Close > EMA and Supertrend bullish" if signal == "BUY" else "Close < EMA or Supertrend bearish"
     return signal, reason, ema
