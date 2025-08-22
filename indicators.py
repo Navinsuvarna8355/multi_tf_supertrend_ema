d2f6ca1882bbca29a53a6b32987a8a6ea7573b77
Navinsuvarna8355 <156652548+Navinsuvarna8355@users.noreply.github.com>
2025-08-22 14:12:55 +0530

    Update indicators.py

--- indicators.py
+++ indicators.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import ta
+
+def add_indicators(df):
+    df["EMA"] = ta.trend.ema_indicator(df["close"], window=20)
+    df["Supertrend"] = ta.trend.stc(df["close"], fillna=True)
+    return df
+
+def generate_signals(df):
+    df["Signal"] = "Hold"
+    df.loc[(df["close"] > df["EMA"]) & (df["Supertrend"] > df["close"]), "Signal"] = "Buy"
+    df.loc[(df["close"] < df["EMA"]) & (df["Supertrend"] < df["close"]), "Signal"] = "Sell"
+    return df
 
